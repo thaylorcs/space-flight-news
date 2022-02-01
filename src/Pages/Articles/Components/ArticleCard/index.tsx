655d802df9d1b581c2099b62f4ed577b24ec1acd
Thaylor Carvalho <thaylorcs@outlook.com>
2022-02-01 12:00:35 -0300

    Formated Date, adjust date line layout

--- src/Pages/Articles/Components/ArticleCard/index.tsx
+++ src/Pages/Articles/Components/ArticleCard/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Article } from '../../../../core/Types/article';
 import './style.css';
 
@@ -6,15 +7,25 @@ type Props = {
 }
 
 const ArticleCard = ({ article }: Props) => {
+    const [articleDate, setArticleDate] = useState('');
+
+    useEffect(() => {
+        const date = new Date(article.publishedAt);
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        setArticleDate(`${day}/${month}/${year}`);
+    }, [article.publishedAt])
+
     return (
-        <article className={article.id % 2 === 0 ? "article-even" : "article-odd"}>
+        <article className={article.id % 2 === 0 ? "article-even" : "article-odd"} key={article.id}>
             <div className="article-img">
                 <img className="article-img" src={article.imageUrl} alt={article.title} />
             </div>
             <div className="article-text" >
                 <h2 className="article-title">{article.title}</h2>
                 <div className="date-source">
-                    <p className='article-date'> {article.publishedAt}</p>
+                    <p className='article-date'> {articleDate}</p>
                     <a href={article.url} target="_blank" className="btn btn-primary btn-source" rel="noreferrer">{article.newsSite}</a>
                 </div>
                 <p className="article-content">{article.summary}</p>
