4b34ab737a9f35e8019ef20df0aecda28753d854
Thaylor Carvalho <thaylorcs@outlook.com>
2022-02-02 08:22:28 -0300

    Added Loader to the main page

---
+++ src/Pages/Articles/Components/CardLoader/index.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import ContentLoader from "react-content-loader";
+
+const CardLoader = () => {
+    const loaderItems = [0, 1, 2, 3, 4];
+
+    return (
+        <>
+
+            {loaderItems.map(item => (
+                <ContentLoader
+                    key={item}
+                    speed={2}
+                    width={800}
+                    height={290}
+                    viewBox="0 0 400 150"
+                    backgroundColor="#ecebeb"
+                    foregroundColor="#d6d2d2"
+                >
+                    <rect x="0" y="25" rx="0" ry="0" width="150" height="119" />
+                    <rect x="190" y="30" rx="0" ry="0" width="240" height="25" />
+                    <rect x="190" y="80" rx="0" ry="0" width="210" height="10" />
+                    <rect x="190" y="100" rx="0" ry="0" width="210" height="10" />
+                    <rect x="190" y="120" rx="0" ry="0" width="210" height="10" />
+                </ContentLoader>
+            ))}
+        </>
+    )
+}
+
+export default CardLoader;
+
--- src/Pages/Articles/index.tsx
+++ src/Pages/Articles/index.tsx
@@ -3,19 +3,25 @@ import Filters from '../../core/components/Filters';
 import { Article } from '../../core/Types/article';
 import { makeRequest } from '../../core/Utils/request';
 import ArticleCard from './Components/ArticleCard';
+import CardLoader from './Components/CardLoader';
 import './style.css';
 
 const Articles = () => {
 
     const [articlesResponse, setArticlesResponse] = useState<Article[]>();
     const [articlesLimit, setArticlesLimit] = useState(10);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const params = {
             _limit: articlesLimit,
         }
+        setIsLoading(true);
         makeRequest({ url: "/articles", params })
-            .then(response => setArticlesResponse(response.data));
+            .then(response => setArticlesResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            })
     }, [articlesLimit]);
 
     const loadMore = () => {
@@ -33,9 +39,11 @@ const Articles = () => {
                 handleSortOrder={handleSortOrder}
             />
             <main className='main-content container-fluid'>
-                {articlesResponse?.map(article => (
-                    <ArticleCard article={article} />
-                ))}
+                {isLoading ? <CardLoader /> : (
+                    articlesResponse?.map(article => (
+                        <ArticleCard article={article} />
+                    ))
+                )}
                 <button onClick={loadMore} className="btn btn-primary carregar-mais">
                     Carregar Mais
                 </button>
